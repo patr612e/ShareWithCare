8426919afd0c27dd5433c8e751ea003f0fbe3426
patr612e <44566563+patr612e@users.noreply.github.com>
2018-11-16 11:32:11 +0100

    Javascript

---
+++ script.js
@@ -0,0 +1,172 @@
+//alert("testing")
+
+window.addEventListener("load", sidenVises);
+
+
+function sidenVises() {
+    console.log("sidenVises");
+    showStart();
+
+}
+
+function showStart {
+    console.log("showStart");
+
+}
+
+function hideStart {
+    console.log("hideStart")
+
+}
+
+function showInstructions {
+    console.log("showInstructions")
+
+}
+
+function hideInstructions {
+    console.log("hideInstructions")
+
+}
+
+
+//********* SETTINGS ******************
+
+
+//********* SETTINGS SLUT **************
+
+
+
+
+
+//*********** EMAIL_1 *************************
+
+
+function nyMail_1 {
+    console.log("nyMail_1")
+}
+
+function clickSlet_1 {
+    console.log("clickSlet_1")
+}
+
+function clickGem_1 {
+    console.log("clickGem_1")
+}
+
+function clickVideresend_1 {
+    console.log("clickVideresend_1")
+}
+
+function visForkert_1 {
+    console.log("visForkert_1")
+}
+
+function visKorrekt_1 {
+    console.log("visKorrekt_1")
+}
+
+function clickVidere_1 {
+    console.log("clickVidere_1")
+}
+
+
+
+//*********** EMAIL_1 SLUT ********************
+
+
+
+
+
+
+
+
+//*********** EMAIL_2 *************************
+
+
+function nyMail_2 {
+    console.log("nyMail_2")
+}
+
+function clickSlet_2 {
+    console.log("clickSlet_2")
+}
+
+function clickGem_2 {
+    console.log("clickGem_2")
+}
+
+function clickVideresend_2 {
+    console.log("clickVideresend_2")
+}
+
+function visForkert_2 {
+    console.log("visForkert_2")
+}
+
+function visKorrekt_2 {
+    console.log("visKorrekt_2")
+}
+
+function clickVidere_2 {
+    console.log("clickVidere_2")
+}
+
+
+//*********** EMAIL_2 SLUT ********************
+
+
+
+
+
+//*********** EMAIL_3 *************************
+
+
+function nyMail_3 {
+    console.log("nyMail_3")
+}
+
+function clickSlet_3 {
+    console.log("clickSlet_3")
+}
+
+function clickGem_3 {
+    console.log("clickGem_3")
+}
+
+function clickVideresend_3 {
+    console.log("clickVideresend_3")
+}
+
+function visForkert_3 {
+    console.log("visForkert_3")
+}
+
+function visKorrekt_3 {
+    console.log("visKorrekt_3")
+}
+
+function clickVidere_3 {
+    console.log("clickVidere_3")
+}
+
+
+//*********** EMAIL_3 SLUT ********************
+
+
+
+//************* GAMEOVER ************************
+
+function visFaengsel {
+    console.log("visFaengsel")
+}
+
+function visPlet {
+    console.log("visPlet")
+}
+
+function visFri {
+    console.log("visFri")
+}
+
+//************* GAMEOVER SLUT *******************
