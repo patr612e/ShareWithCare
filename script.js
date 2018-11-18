2ae43fcead8eca21342e224f4e8e086bbf562df9
Sophia Svane <sophiasvane@Sophias-MacBook-Pro.local>
2018-11-18 18:41:27 +0100

    Mangler slutskærme og lyd

--- script.js
+++ script.js
@@ -2,47 +2,163 @@
 
 window.addEventListener("load", sidenVises);
 
+let showSettingsMusic = true;
+let showSettingsSound = true;
 
 function sidenVises() {
     console.log("sidenVises");
+
+    document.querySelector("#spiligen1").removeEventListener("click", sidenVises);
+    document.querySelector("#spiligen2").removeEventListener("click", sidenVises);
+    document.querySelector("#spiligen3").removeEventListener("click", sidenVises);
+
     showStart();
 
 }
 
-function showStart {
+function showStart() {
     console.log("showStart");
+    document.querySelector("#start").classList.remove("hide");
+    document.querySelector("#start_screen").classList.remove("hide");
 
-}
-
-function hideStart {
-    console.log("hideStart")
-
-}
-
-function showInstructions {
-    console.log("showInstructions")
-
-}
-
-function hideInstructions {
-    console.log("hideInstructions")
+    document.querySelector("#settings_button").addEventListener("click", showSettings);
+    document.querySelector("#play_button").addEventListener("click", hideStart);
 
 }
 
 
 //********* SETTINGS ******************
 
+function showSettings() {
+    console.log("showSettings");
+
+    document.querySelector("#settings_button").removeEventListener("click", showSettings);
+
+    document.querySelector("#settings").classList.remove("hide");
+
+    document.querySelector("#settings_music").addEventListener("click", toggleMusic);
+    document.querySelector("#settings_sound").addEventListener("click", toggleSound);
+
+    document.querySelector("#close_settings").addEventListener("click", hideSettings);
+}
+
+
+function toggleMusic() {
+    console.log("toggleMusic");
+
+    if (showSettingsMusic == true) {
+        showSettingsMusic = false;
+        document.querySelector("#music_sprite").classList.add("on_off");
+        document.querySelector("#music_sprite").classList.remove("on");
+        document.querySelector("#music_sprite").addEventListener("animationend", musicOff);
+    } else {
+        showSettingsMusic = true;
+        document.querySelector("#music_sprite").classList.add("off_on");
+        document.querySelector("#music_sprite").classList.remove("off");
+
+        document.querySelector("#music_sprite").addEventListener("animationend", musicOn);
+    }
+}
+
+function musicOff() {
+    console.log("musicOff function værdi er" + showSettingsMusic);
+    document.querySelector("#music_sprite").removeEventListener("animationend", musicOff);
+
+    document.querySelector("#music_sprite").classList.remove("on_off");
+    document.querySelector("#music_sprite").classList.add("off");
+
+    // document.querySelector("#music_mp3").pause();
+}
+
+function musicOn() {
+    console.log("musicOn function værdi er" + showSettingsMusic);
+    document.querySelector("#music_sprite").removeEventListener("animationend", musicOn);
+
+    document.querySelector("#music_sprite").classList.remove("off_on");
+    document.querySelector("#music_sprite").classList.add("on");
+
+    // document.querySelector("#music_mp3").play();
+}
+
+
+function toggleSound() {
+    console.log("toggleSound");
+
+    if (showSettingsSound == true) {
+        showSettingsSound = false;
+        document.querySelector("#sound_sprite").classList.add("on_off");
+        document.querySelector("#sound_sprite").classList.remove("on");
+        document.querySelector("#sound_sprite").addEventListener("animationend", soundOff);
+    } else {
+        showSettingsSound = true;
+        document.querySelector("#sound_sprite").classList.add("off_on");
+        document.querySelector("#sound_sprite").classList.remove("off");
+
+        document.querySelector("#sound_sprite").addEventListener("animationend", soundOn);
+    }
+}
+
+function soundOff() {
+    console.log("soundOff function værdi er" + showSettingsSound);
+    document.querySelector("#sound_sprite").removeEventListener("animationend", soundOff);
+
+    document.querySelector("#sound_sprite").classList.remove("on_off");
+    document.querySelector("#sound_sprite").classList.add("off");
+
+    //document.querySelector("#sound_mp3").muted = true;
+}
+
+function soundOn() {
+    console.log("soundOn function værdi er" + showSettingsSound);
+    document.querySelector("#sound_sprite").removeEventListener("animationend", soundOn);
+
+    document.querySelector("#sound_sprite").classList.remove("off_on");
+    document.querySelector("#sound_sprite").classList.add("on");
+
+    //document.querySelector("#sound_mp3").muted = false;
+}
+
+
+function hideSettings() {
+    console.log("hideSettings");
+    document.querySelector("#close_settings").removeEventListener("click", hideSettings);
+    document.querySelector("#settings").classList.add("hide");
+
+    showStart();
+}
 
 //********* SETTINGS SLUT **************
 
 
+function hideStart() {
+    console.log("hideStart")
+    document.querySelector("#start").classList.add("hide");
 
+    showInstructions();
+}
+
+function showInstructions() {
+    console.log("showInstructions")
+    document.querySelector("#intro").classList.remove("hide");
+    document.querySelector("#videre_knap").addEventListener("click", hideInstructions);
+
+}
+
+function hideInstructions() {
+    console.log("hideInstructions")
+    document.querySelector("#videre_knap").removeEventListener("click", hideInstructions);
+
+    document.querySelector("#intro").classList.add("hide");
+    document.querySelector("#start_screen").classList.add("hide");
+
+    nyMail_1();
+}
 
 
 //*********** EMAIL_1 *************************
 
 
-function nyMail_1 {
+function nyMail_1() {
     console.log("nyMail_1");
 
     document.querySelector("#game_background1").classList.remove("hide");
@@ -54,10 +170,14 @@ function nyMail_1 {
 }
 
 
-function visForkert_1 {
+function visForkert_1() {
     console.log("visForkert_1");
 
+    document.querySelector("#gem1").removeEventListener("click", visForkert_1);
+    document.querySelector("#videresend1").removeEventListener("click", visForkert_1);
+
     document.querySelector("#game_background1").classList.add("blur");
+    document.querySelector("#game_elements1").classList.add("blur");
     document.querySelector("#forkert1").classList.remove("hide");
     document.querySelector("#videre1").classList.remove("hide");
 
@@ -65,18 +185,23 @@ function visForkert_1 {
     document.querySelector("#videre1").addEventListener("click", hideNyMail_1);
 }
 
-function visKorrekt_1 {
+function visKorrekt_1() {
     console.log("visKorrekt_1");
 
+    document.querySelector("#slet1").removeEventListener("click", visKorrekt_1);
+
     document.querySelector("#game_background1").classList.add("blur");
+    document.querySelector("#game_elements1").classList.add("blur");
     document.querySelector("#korrekt1").classList.remove("hide");
     document.querySelector("#videre1").classList.remove("hide");
 
     document.querySelector("#videre1").addEventListener("click", hideNyMail_1);
 }
 
-function hideNyMail_1 {
+function hideNyMail_1() {
     console.log("hideNyMail_1");
+
+    document.querySelector("#videre1").removeEventListener("click", hideNyMail_1);
 
     document.querySelector("#game_background1").classList.add("hide");
     document.querySelector("#game_elements1").classList.add("hide");
@@ -91,17 +216,10 @@ function hideNyMail_1 {
 
 //*********** EMAIL_1 SLUT ********************
 
-
-
-
-
-
-
-
 //*********** EMAIL_2 *************************
 
 
-function nyMail_2 {
+function nyMail_2() {
     console.log("nyMail_2")
 
     document.querySelector("#game_background2").classList.remove("hide");
@@ -113,70 +231,146 @@ function nyMail_2 {
 }
 
 
-function visForkert_2 {
+function visForkert_2() {
     console.log("visForkert_2")
+
+    document.querySelector("#gem2").removeEventListener("click", visForkert_2);
+    document.querySelector("#videresend2").removeEventListener("click", visForkert_2);
+
+    document.querySelector("#game_background2").classList.add("blur");
+    document.querySelector("#game_elements2").classList.add("blur");
+    document.querySelector("#forkert2").classList.remove("hide");
+    document.querySelector("#videre2").classList.remove("hide");
+
+
+    document.querySelector("#videre2").addEventListener("click", hideNyMail_2);
+
 }
 
-function visKorrekt_2 {
+function visKorrekt_2() {
     console.log("visKorrekt_2")
+
+    document.querySelector("#slet2").removeEventListener("click", visKorrekt_2);
+
+    document.querySelector("#game_background2").classList.add("blur");
+    document.querySelector("#game_elements2").classList.add("blur");
+    document.querySelector("#korrekt2").classList.remove("hide");
+    document.querySelector("#videre2").classList.remove("hide");
+
+    document.querySelector("#videre2").addEventListener("click", hideNyMail_2);
 }
 
+function hideNyMail_2() {
+    console.log("hideNyMail_1");
 
+    document.querySelector("#videre2").removeEventListener("click", hideNyMail_2);
+
+    document.querySelector("#game_background2").classList.add("hide");
+    document.querySelector("#game_elements2").classList.add("hide");
+    document.querySelector("#forkert2").classList.add("hide");
+    document.querySelector("#korrekt2").classList.add("hide");
+    document.querySelector("#videre2").classList.add("hide");
+
+    nyMail_3();
+
+}
 
 //*********** EMAIL_2 SLUT ********************
-
-
-
-
 
 //*********** EMAIL_3 *************************
 
 
-function nyMail_3 {
+function nyMail_3() {
     console.log("nyMail_3")
+
+    document.querySelector("#game_background3").classList.remove("hide");
+    document.querySelector("#game_elements3").classList.remove("hide")
+
+
+    document.querySelector("#gem3").addEventListener("click", visKorrekt_3);
+    document.querySelector("#slet3").addEventListener("click", visForkert_3);
+    document.querySelector("#videresend3").addEventListener("click", visForkert_3);
 }
 
-function clickSlet_3 {
-    console.log("clickSlet_3")
-}
-
-function clickGem_3 {
-    console.log("clickGem_3")
-}
-
-function clickVideresend_3 {
-    console.log("clickVideresend_3")
-}
-
-function visForkert_3 {
+function visForkert_3() {
     console.log("visForkert_3")
+
+    document.querySelector("#slet3").removeEventListener("click", visForkert_3);
+    document.querySelector("#videresend3").removeEventListener("click", visForkert_3);
+
+    document.querySelector("#game_background3").classList.add("blur");
+    document.querySelector("#game_elements3").classList.add("blur");
+    document.querySelector("#forkert3").classList.remove("hide");
+    document.querySelector("#videre3").classList.remove("hide");
+
+
+    document.querySelector("#videre3").addEventListener("click", hideNyMail_3);
 }
 
-function visKorrekt_3 {
+function visKorrekt_3() {
     console.log("visKorrekt_3")
+
+    document.querySelector("#gem3").removeEventListener("click", visKorrekt_3);
+
+    document.querySelector("#game_background3").classList.add("blur");
+    document.querySelector("#game_elements3").classList.add("blur");
+    document.querySelector("#korrekt3").classList.remove("hide");
+    document.querySelector("#videre3").classList.remove("hide");
+
+    document.querySelector("#videre3").addEventListener("click", hideNyMail_3);
 }
 
-function clickVidere_3 {
-    console.log("clickVidere_3")
+function hideNyMail_3() {
+    console.log("hideNyMail_1");
+
+    document.querySelector("#videre3").removeEventListener("click", hideNyMail_3);
+
+    document.querySelector("#game_background3").classList.add("blur");
+    document.querySelector("#game_elements3").classList.add("blur");
+    document.querySelector("#forkert3").classList.add("hide");
+    document.querySelector("#korrekt3").classList.add("hide");
+    document.querySelector("#videre3").classList.add("hide");
+
+    gameStatus();
+
 }
 
 
 //*********** EMAIL_3 SLUT ********************
 
+//************ GAME STATUS *******************
 
+function gameStatus() {
+
+    // if else point
+
+    // visFaengsel();
+    // visFri();
+    // visPlet();
+
+}
 
 //************* GAMEOVER ************************
 
-function visFaengsel {
+function visFaengsel() {
     console.log("visFaengsel")
+    document.querySelector("#faengsel").classList.remove("hide");
+
+    document.querySelector("#spiligen1").addEventListener("click", sidenVises);
 }
 
-function visPlet {
-    console.log("visPlet")
-}
-
-function visFri {
+function visFri() {
     console.log("visFri")
+    document.querySelector("#fri").classList.remove("hide");
+
+    document.querySelector("#spiligen2").addEventListener("click", sidenVises);
+}
+
+function visPlet() {
+    console.log("visPlet")
+    document.querySelector("#plet").classList.remove("hide");
+
+    document.querySelector("#spiligen3").addEventListener("click", sidenVises);
 }
 
 //************* GAMEOVER SLUT *******************
